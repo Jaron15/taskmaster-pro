13f049d2eb41a1bd4e74be7a64e7319a29b08c88
Jaron <jjkenyon777@gmail.com>
2021-08-25 17:13:32 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,7 +66,7 @@ $("#modalDueDate").datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -198,18 +198,20 @@ $("#remove-tasks").on("click", function() {
    scroll: false, 
    tolerance: "pointer",
    helper: "clone",
-  //  activate: function(event) {
-  //    console.log("activate", this);
-  //  },
-  //  deactivate: function(event) {
-  //    console.log("deactivate", this);
-  //  },
-  //  over: function(event) {
-  //    console.log("over",event.target);
-  //  },
-  //  out: function(event) {
-  //    console.log("out", event.target);
-  //  },
+    activate: function(event) {
+      $(this).addClass("dropover")
+      $(".bottom-trash").addClass("bottom-trash-drag")
+    },
+    deactivate: function(event) {
+     $(this).removeClass("dropover")
+     $(".bottom-trash").removeClass("bottom-trash-drag")
+     },
+    over: function(event) {
+      $(event.target).addClass("dropover-active")
+    },
+    out: function(event) {
+      $(event.target).removeClass("dropover-active")
+    },
    update: function(event) {
      // array to store the task data in
      var tempArr = [];
@@ -248,12 +250,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function(event, ui) {
     console.log("out")
+    $(".bottom-trash").removeClass("bottom-trash-active")
   }
 });
  var auditTask = function(taskEl) {
@@ -273,9 +278,16 @@ $("#trash").droppable({
    else if (Math.abs(moment().diff(time, "days")) <= 2) {
      $(taskEl).addClass("list-group-item-warning");
    }
+   console.log(taskEl);
  };
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
